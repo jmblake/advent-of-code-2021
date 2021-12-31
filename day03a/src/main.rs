aec8db2421b0a58d56313afd3674c91158ff483d
jmblake <jmblake96@gmail.com>
2021-12-31 20:08:04 +0000

    Add input data

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -11,10 +11,17 @@ fn main() {
         .collect();
     let mut counts: [u32; NUM_BITS] = [0; NUM_BITS];
     for i in 0..NUM_BITS {
-        counts[i] = input.iter().filter(|&s| s.chars().nth(i).unwrap() == '1').count() as u32;
+        counts[i] = input
+            .iter()
+            .filter(|&s| s.chars().nth(i).unwrap() == '1')
+            .count() as u32;
     }
-    let gamma_str = counts.iter().map(|&x| if x > NUM_ROWS / 2 {"1"} else {"0"}).collect::<Vec<_>>().join("");
+    let gamma_str = counts
+        .iter()
+        .map(|&x| if x > NUM_ROWS / 2 { "1" } else { "0" })
+        .collect::<Vec<_>>()
+        .join("");
     let gamma = u32::from_str_radix(&gamma_str, 2).unwrap();
     let epsilon = !gamma & ((1 << NUM_BITS) - 1); // https://stackoverflow.com/a/36124832
-    println!("{}", gamma * epsilon);
+    println!("{}", gamma * epsilon); // 2954600
 }
